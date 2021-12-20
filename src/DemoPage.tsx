d2b116feaa7cffbfa9c61ca90e5863d5bbcdfd50
Peter Vivo <peter.vivo@appercell.com>
2021-12-20 16:01:56 +0100

    input

--- src/DemoPage.tsx
+++ src/DemoPage.tsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
+
+type StringFunction = (a: String) => void;
+const withInputValue = (action:StringFunction) => ({target:{value}}) => action(value);
+
 export default () => {
 
+  const [foo, setFoo] = useState("");
+
   return (
-    <pre>{`
-
-    - typescript
-    - react
-    - react-Troll
-    - use-saga-reducer
-    - local mongo db
-
-    `}</pre>
+    <pre>
+      {`under consturct ...`}
+      <br/>
+      <br/>
+      <input value={foo} placeholder="some text" onChange={withInputValue(setFoo)}/>
+      <p>foo: {foo}</p>
+    </pre>
   );
 }
